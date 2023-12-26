023b3d9049313de47fa94fe7612d34e1fe53d1ce
jeonsujeong <jsj93@ppsystem.com>
2023-12-26 12:26:22 +0900

    init commit

--- src/main/java/org/kPaas/kdi/KdiApplication.java
+++ src/main/java/org/kPaas/kdi/KdiApplication.java
@@ -3,7 +3,7 @@ package org.kPaas.kdi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
 public class KdiApplication {
 
 	public static void main(String[] args) {
