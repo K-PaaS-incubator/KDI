2fa47d825c086f0d0e1a01f2384f456a3050cfca
양시열 <xosy127@ppsystem.co.kr>
2024-02-18 13:03:06 +0900

    .

--- src/main/java/org/kPaas/kdi/main/datasource/mapper/DatasourceMapper.java
+++ src/main/java/org/kPaas/kdi/main/datasource/mapper/DatasourceMapper.java
@@ -1,7 +1,5 @@
 package org.kPaas.kdi.main.datasource.mapper;
 
-import java.sql.SQLException;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.kPaas.kdi.main.datasource.dto.DatasourceVo;
 
@@ -9,7 +7,7 @@ import org.kPaas.kdi.main.datasource.dto.DatasourceVo;
 public interface DatasourceMapper {
 
 	// 데이터소스 정보 저장
-	public void insertDS(DatasourceVo datasource_vo) throws SQLException;
+	public void insertDS(DatasourceVo datasource_vo);
 
 	// 데이터소스 테이블 생성
 	public void createTable();
