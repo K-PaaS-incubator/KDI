098ce329491c923ad1f256501879c1a380db29b4
전수정 <jsj93@ppsystem.co.kr>
2024-03-21 12:25:52 +0900

    데이터소스 동적 변경기능 추가

--- src/main/java/org/kPaas/kdi/com/config/KdiDBConfig.java
+++ src/main/java/org/kPaas/kdi/com/config/KdiDBConfig.java
@@ -6,18 +6,25 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @MapperScan(value = "org.kPaas.kdi.**.mapper", sqlSessionFactoryRef = "sqlSessionFactory")
-public class MybatisConfig {
+@EnableTransactionManagement(proxyTargetClass = true, mode = AdviceMode.PROXY) // <tx:annotation-driven>
+public class KdiDBConfig {
+	@Autowired
+	private ApplicationContext context;
 
-	private final ApplicationContext context;
-
-	public MybatisConfig(ApplicationContext context) {
-		this.context = context;
+	@Bean
+	DataSource dataSource(KdiRoutingDataSource kdiRoutingDataSource) {
+		return kdiRoutingDataSource;
 	}
 
 	@Bean
@@ -25,6 +32,7 @@ public class MybatisConfig {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
 		sqlSessionFactoryBean.setConfigLocation(context.getResource("classpath:mybatis-config.xml"));
+		// MapperScan에서 명시했음으로 불필요
 		sqlSessionFactoryBean.setMapperLocations(context.getResources("classpath:org/kPaas/kdi/**/mapper/*.xml"));
 
 		return sqlSessionFactoryBean.getObject();
@@ -33,6 +41,11 @@ public class MybatisConfig {
 	@Bean
 	SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
 		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+
+	@Bean
+	TransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
 	}
 
 }
---
+++ src/main/java/org/kPaas/kdi/com/config/KdiRoutingDataSource.java
@@ -0,0 +1,94 @@
+package org.kPaas.kdi.com.config;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
+
+import org.kPaas.kdi.main.datasource.vo.DatasourceVo;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * 동적으로 사용할 DB정보를 제공/등록/수정/삭제하는 기능
+ */
+@Configuration
+public class KdiRoutingDataSource extends AbstractRoutingDataSource {
+	private final Map<Object, Object> kdiDataSourceMap = new ConcurrentHashMap<Object, Object>();
+
+	public KdiRoutingDataSource( // application.properties 정의된 spring.datasource 설정
+			DataSourceProperties dataSourceProperties) {
+		kdiDataSourceMap.put(KdiContextHolder.DEFAULT,
+				dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build());
+		super.setTargetDataSources(kdiDataSourceMap);
+	}
+
+	@Override
+	protected Object determineCurrentLookupKey() {
+		return KdiContextHolder.get();
+	}
+
+	/**
+	 * 동적으로 사용할 DB정보를 등록/수정 기능
+	 * 
+	 * @param dataSourceName
+	 * @param dataSource
+	 */
+	public synchronized void put(DatasourceVo vo) {
+		DataSourceProperties prop = new DataSourceProperties();
+		prop.setUrl(vo.getDs_url());
+		// 현재 KDI코드에서 Ds_driver값이 확인안됨
+		// prop.setDriverClassName(vo.getDs_driver());
+		prop.setUsername(vo.getDs_usr_nm());
+		prop.setPassword(vo.getDs_usr_pw());
+
+		// 데이터소스 호출시 사용될 Key값
+		String dataSourceName = vo.getDs_nm();
+		// 데이터소스 반여작업
+		try {
+			if (!kdiDataSourceMap.containsKey(dataSourceName)) {
+				return;
+			}
+			if (null == kdiDataSourceMap.get(dataSourceName)) {
+				return;
+			}
+			// 기존 데이터소스풀(HikariDataSource) 있으면 Close
+			((HikariDataSource) kdiDataSourceMap.get(dataSourceName)).close();
+		} finally {
+			// 데이터소스 생성
+			DataSource newDataSource = prop.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+
+			// 데이터소스 입력
+			kdiDataSourceMap.put(dataSourceName, newDataSource);
+
+			// 최신화된 kdiDataSourceMap 정보 반영
+			super.afterPropertiesSet();
+		}
+	}
+
+	/**
+	 * 동적으로 사용할 DB정보를 삭제 기능
+	 * 
+	 * @param dataSourceName
+	 * @param dataSource
+	 */
+	public synchronized void remove(final String dataSourceName) {
+		if (!kdiDataSourceMap.containsKey(dataSourceName)) {
+			return;
+		}
+		try {
+			if (null == kdiDataSourceMap.get(dataSourceName)) {
+				return;
+			}
+			((HikariDataSource) kdiDataSourceMap.get(dataSourceName)).close();
+		} finally {
+			kdiDataSourceMap.remove(dataSourceName);
+			// 최신화된 kdiDataSourceMap 정보 반영
+			super.afterPropertiesSet();
+		}
+
+	}
+}
