18efb698f7347e1680adc0ec1e06abf4538b9631
양시열 <xosy127@ppsystem.co.kr>
2024-06-02 14:01:33 +0900

    접근권한 관라자 권한 명시

--- src/main/java/org/kPaas/kdi/main/pop/controller/PopTableController.java
+++ src/main/java/org/kPaas/kdi/main/pop/controller/PopTableController.java
@@ -6,12 +6,14 @@ import org.kPaas.kdi.com.abs.AbstractController;
 import org.kPaas.kdi.main.pop.service.PopTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@PreAuthorize("hasRole('ADMIN')") // 관리자
 @RequestMapping("pop/table")
 public class PopTableController extends AbstractController {
 	@Autowired
